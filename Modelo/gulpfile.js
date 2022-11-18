b19c3e35b383482e560b6d597958132659eec8aa
RayTowns <raysa.carraro@hotmail.com>
2022-11-17 22:05:59 -0300

    incluindo gulpfile no arquivo html

--- Modelo/gulpfile.js
+++ Modelo/gulpfile.js
@@ -3,23 +3,57 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
 
 function tarefasCSS(cb) {
     
-   return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+   return gulp.src([
+        './vendor/bootstrap-5.2.0-dist/css/bootstrap.css',
+        './vendor/Fontawesome/css/all.css',
+        './vendor/jquery-ui/jquery-ui.css',
+        './vendor/owlcarousel/docs/assets/owlcarousel/assets/owl.carousel.css',
+        './vendor/owlcarousel/docs/assets/owlcarousel/assets/owl.theme.default.css',
+        './src/style.css'
+   ])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
 }
 
 function tarefasJS(){
-    return gulp.src('./vendor/**/*.js')
-    .pipe(concat('libs.js'))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min'}))
-    .pipe(gulp.dest('./dist/js'))
+
+    return gulp.src(['./jquery-3.6.0.js',
+        './vendor/Fontawesome/js/all.js',
+        './vendor/bootstrap-5.2.0-dist/js/bootstrap.js',
+        './vendor/owlcarousel/docs/assets/owlcarousel/owl.carousel.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './src/customized.js'
+    ])
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min'}))
+        .pipe(gulp.dest('./dist/js'))
 }
 
-  exports.styles = tarefasCSS()
-  exports.scripts = tarefasJS()
+function tarefasImagem(){
+
+    return gulp.src('./src/imagens/*')
+        .pipe(imagemin({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(rename({ suffix: '.min'}))
+        .pipe(gulp.dest('./dist/images'))
+}
+
+  exports.styles = tarefasCSS
+  exports.scripts = tarefasJS
+  exports.images = tarefasImagem
