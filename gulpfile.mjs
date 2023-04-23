09110ee95a7f2435c2620fe134b4cc35283a31b4
RayTowns <raysa.carraro@hotmail.com>
2023-04-23 15:40:41 -0300

    automatizacao do projeto com gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,10 +5,13 @@ import cssmin from 'gulp-cssmin';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import imagemin from 'gulp-imagemin';
+import htmlmin from 'gulp-htmlmin';
+import babel from 'gulp-babel';
+import browserSync, { reload } from 'browser-sync';
 
-gulp.task("tarefasCSS", function() {
+gulp.task("tarefasCSS", function(callback) {
 
-   return gulp.src([
+    gulp.src([
         './src/vendor/jquery-ui/jquery-ui.css',
         './src/vendor/bootstrap/css/bootstrap.css',
         './src/vendor/Fontawesome/css/all.css',
@@ -20,11 +23,13 @@ gulp.task("tarefasCSS", function() {
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
+
+        return callback()
 });
 
-gulp.task("tarefasJS", function() {
+gulp.task("tarefasJS", function(callback) {
 
-    return gulp.src([
+     gulp.src([
         './jquery-3.6.0.js',
         './src/vendor/jquery-ui/jquery-ui.js',
         './src/vendor/bootstrap/js/bootstrap.js',
@@ -32,18 +37,26 @@ gulp.task("tarefasJS", function() {
         './src/vendor/owlcarousel/docs/assets/owlcarousel/owl.carousel.js',
         './src/vendor/customized.js',
     ])
+
+    .pipe(babel({
+        comments: true,
+        presets: ['@babel/env']
+    }))
+
         .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
+
+        return callback()
 });
 
-gulp.task("tarefasImagem", function() {
+gulp.task("tarefasImagem", function(callback) {
 
     let src = '/src/imagens/*';
     let dest = '/dist/images';
 
-    return gulp.src(src)
+     gulp.src(src)
         .pipe(imagemin({
             pngquant: true,
             optipng: false,
@@ -57,4 +70,28 @@ gulp.task("tarefasImagem", function() {
         }))
         .pipe(rename({ suffix: '.min'}))
         .pipe(gulp.dest(dest))
+
+        return callback()
 });
+
+gulp.task("tarefasHTML", function(callback){
+
+     gulp.src('./src/**/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'))
+
+    return callback()
+});
+
+gulp.task("serve", function(){
+    browserSync.init({
+        server:{
+            baseDir: "./dist"
+        }
+    })
+
+    gulp.watch("./src/**/*").on("change", gulp.task('sync'))
+    gulp.watch("./src/**/*").on("change", reload)
+})
+
+gulp.task ('sync', gulp.parallel( 'tarefasHTML', 'tarefasCSS', 'tarefasJS' ))
